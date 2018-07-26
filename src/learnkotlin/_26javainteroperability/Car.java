b9a5e02ea82b61a5c6871f4b8a168e0cec33ddb4
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2018-07-26 18:37:49 +0100

    lecture on java interop

---
+++ src/learnkotlin/_26javainteroperability/Car.java
@@ -0,0 +1,47 @@
+package learnkotlin._26javainteroperability;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+
+public class Car {
+
+    private String color;
+    private String model;
+    private int year;
+
+    public Car(String color, String model, int year) {
+        this.color = color;
+        this.model = model;
+        this.year = year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(@NotNull String color) {
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public @Nullable int getYear() {
+        return year;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "color='" + color + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}
